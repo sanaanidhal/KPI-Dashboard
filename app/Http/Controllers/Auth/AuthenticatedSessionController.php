e2fdb4defc92bad6f497f19c30fbb0ad949e3206
sanaanidhal <nidhalsanaa11@supcom.tn>
2024-04-15 18:46:33 +0100

    bug fix

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -51,6 +51,6 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
         
 
-        return redirect('/admin/login');
+        return redirect('/login');
     }
 }
